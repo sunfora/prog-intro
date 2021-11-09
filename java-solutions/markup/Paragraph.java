ff967d9c348606cfccf2c4d7ff5e78b33d840bad
kurku <sandovin@mail.ru>
2021-11-09 03:57:55 +0300

    hw7 added

--- java-solutions/markup/Paragraph.java
+++ java-solutions/markup/Paragraph.java
@@ -1,2 +1,26 @@
-public class Paragraph {
+package markup;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Paragraph implements BBCodeable {
+
+    private final List<ParagraphElement> elems;
+
+    public Paragraph(Iterable<ParagraphElement> vars) {
+        elems = new ArrayList<ParagraphElement>();
+        for (ParagraphElement var : vars) {
+            elems.add(var);
+        }
+    }
+
+    public Paragraph(ParagraphElement... vars) {
+        this(List.of(vars));
+    }
+
+    public void toBBCode(StringBuilder dest) {
+        for (ParagraphElement elem : elems) {
+            elem.toBBCode(dest);
+        }
+    }
 }
