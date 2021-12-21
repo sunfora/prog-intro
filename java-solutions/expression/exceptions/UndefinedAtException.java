2cf618a24c9619099137e1c64d2a8983879dbceb
kurku <sandovin@mail.ru>
2021-12-21 22:59:44 +0300

    fuck it

--- java-solutions/expression/exceptions/UndefinedAtException.java
+++
@@ -1,7 +0,0 @@
-package expression.exceptions;
-
-public class UndefinedAtException extends ArithmeticException {
-    public UndefinedAtException(String message) {
-        super(message);
-    }
-}
